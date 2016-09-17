730f490ba9088f934c15216e3bc50fc5fdef3cfb
Shin Jinwoo <bluesky0255@gmail.com>
2016-09-17 17:48:15 +0900

    querying filter coditions added

--- routes/event.js
+++ routes/event.js
@@ -8,13 +8,41 @@ var util = require("./utils");
 
 var pool = mysql.createPool(config.db);
 
-
+/**
+ * 게시물 조회 API
+ * 
+ * Path : /api/events
+ * Method : GET
+ * 
+ * Request
+ * @query.from : 전달된 값보다 큰 cultcode 만 조회(기본값 0)
+ * @query.limit : 조회 결과의 상위 일부를 반환(기본값 10)
+ * @query.genre : 조회할 장르 (기본값 없음)
+ * @query.subjcode : subjcode로 조회
+ * 
+ */
 function retrieveEvents(req, res)
 {
 	var result = {};
-	var query = "SELECT * FROM events LIMIT 10";
+	var limit = req.query.limit ? Number(req.query.limit) : 10;
+	var from = req.query.from ? req.query.from : 0;
+	var genre = req.query.genre;
+	var subjcode = req.query.subjcode;
+	var query = "SELECT * FROM events ";
 
-	pool.query(query, function(_err, fields)
+	// make query
+	query += "WHERE cultcode > ? ";
+	if(genre) query += "AND codename=? ";
+	if(subjcode) query += "AND subjcode=? ";
+	query += "LIMIT ?";
+	query += ";";
+	var conds = [];
+	conds.push(from);
+	if(genre){conds.push(genre);}
+	if(subjcode){conds.push(subjcode);}
+	conds.push(limit);
+
+	pool.query(query, conds, function(_err, fields)
 	{
 		if(_err)
 		{
